22bf2fe2c0c03b1a270d3508c119b3c4dda5efed
David Miller <david.miller@showpass.com>
2020-01-17 15:51:01 -0700

    initial functionality

--- src/scripts/rmr-porter.js
+++ src/scripts/rmr-porter.js
@@ -14,38 +14,92 @@
   const
   RMR = require('rmr-util'),
   ATTRS = {
+    heightOffset: 200,
     attr: 'data-rmr-ported'
-  };
+  },
+  inView = function(elem) {
 
-  // being viewed on a touch device?
-  // TOUCH = RMR.Browser.isTouch();
+    const rect = elem.getBoundingClientRect();
+
+    // true if any of the following conditions are met:
+    return(
+      // top is in view: the top is more than 0 and less than the window height (the top of the element is in view)
+      ( (rect.top + ATTRS.heightOffset) >= 0 && (rect.top + ATTRS.heightOffset) <= window.innerHeight ) || 
+      // bottom is in view: bottom position is greater than 0 and greater than the window height
+      ( (rect.bottom + ATTRS.heightOffset) >= 0 && (rect.bottom + ATTRS.heightOffset) <= window.innerHeight ) ||
+      // top is above the viewport and the bottom is below the viewport
+      ( (rect.top + ATTRS.heightOffset) < 0 && (rect.bottom + ATTRS.heightOffset) > window.innerHeight )
+    );
+  };
 
   /**
 
      @param options {Object} - selector/callback pairs to invoke when the elements corresponding to the selectors enter the viewport
    */
-  const Porter = function(elements) {
+  const Porter = function(args) {
+
+    const
+      elements = args,
+      self = this;
+    let nodes = [];
+
+    const viewChange = function() {
+
+      for (const i in elements) {
+        if (! RMR.Object.has(elements, i)) {
+          continue;
+        }
+
+        nodes = RMR.Node.getAll(i);
+        if (! nodes.length) {
+          delete elements[i];
+          console.error('Invalid porter selector', i);
+          continue;
+        }
+
+        nodes.map((n) => {
+          if (! n.getAttribute(ATTRS.attr)) {
+            // element has entered the viewport for the first time, invoke callback
+            if (inView(n)) {
+              elements[i](n);
+              n.setAttribute(ATTRS.attr, true);
+            }
+          }
+        });
+      }
+    };
+
+    addEventListener('scroll', viewChange, false); 
+    addEventListener('resize', viewChange, false);
 
     for (const i in elements) {
       if (! RMR.Object.has(elements, i)) {
         continue;
       }
 
-      const nodes = RMR.Node.getAll(i);
+      nodes = RMR.Node.getAll(i);
       if (! nodes.length) {
+        delete elements[i];
         console.error('Invalid porter selector', i);
         continue;
       }
 
       nodes.map((n) => {
 
-        // element has entered the viewport for the first time, invoke callback
-        if (! n.getAttribute(ATTRS.attr)) {
-          elements[i](n);
-          n.setAttribute(ATTRS.attr, true);
-        }
+        addEventListener('load', (function(key, node) {
+
+          if (inView(node)) {
+            elements[key](node);
+            node.setAttribute(ATTRS.attr, true);
+          }
+        })(i,n), false );
+
       });
     }
+  };
+
+  Porter.prototype.toString = function() {
+    return '🧲 Porter';
   };
 
   module.exports = Porter;
