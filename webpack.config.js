67356fe48cc1337bdb46f4611b9756d1935f1ff9
David Miller <david.miller@showpass.com>
2020-01-28 14:46:40 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const
     path = require('path'),
     webpack = require('webpack'),
     terser = require('terser-webpack-plugin');
-//    uglifyJsPlugin = require('uglifyjs-3-webpack-plugin');
 
 const config = {
   entry: './src/scripts/build.js',
@@ -11,34 +10,23 @@ const config = {
     path: path.resolve(__dirname, 'docs/build/'),
     filename: 'rmr-porter.bundle.js'
   },
-//  mode: 'production',
-  mode: 'development',
+  mode: 'production',
   watch: true,
   plugins : [
     new terser({
       extractComments: false,
       test: /\.js(\?.*)?$/i
     }),
-//     new uglifyJsPlugin({
-//       uglifyOptions: {
-//         warnings: false,
-//         ie8: false,
-//         output: {
-//           comments: false
-//         }
-//       }
-//     })
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
-//        include: path.resolve(__dirname, 'src'),
         use: [{
           loader: 'babel-loader',
           options: {
             presets: [
-              ['es2015']
+              ['env']
             ]
           }
         }]
