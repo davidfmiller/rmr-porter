bd99667c241c263db9dc8fbe721376f8d4183e4d
David Miller <davidfmiller@me.com>
2026-09-12 12:29:24 -0700

    0.2.7

--- webpack.config.js
+++ webpack.config.js
@@ -20,17 +20,6 @@ const config = {
   ],
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['es2015']
-            ]
-          }
-        }]
-      }
     ]
   }
 };
